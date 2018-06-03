a9527d061bdbeb93392444026fdf2201ac1bdcfa
Worameth Semapat <worameth.semapat@gmail.com>
2018-06-03 13:17:31 +0700

    Remove some useless line

--- SocketAsync/HaxtivitiezSocketAsync/HaxtivitiezSocketAsync.cs
+++ SocketAsync/HaxtivitiezSocketAsync/HaxtivitiezSocketAsync.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HaxtivitiezSocketAsync
 {
